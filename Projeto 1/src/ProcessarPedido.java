b3b8d4e5d7f4e3ad9172f1fbcdeb480ede47367d
Pedro Armengol de Oliveira <peronu376@gmail.com>
2025-09-15 16:05:27 -0300

    Delete Projeto 1 directory

--- Projeto 1/src/ProcessarPedido.java
+++
@@ -1,8 +0,0 @@
-import br.inatel.computadores.Computador;
-
-public class ProcessarPedido {
-    public static void processarPedido(Computador[] pcs, int qtd) {
-        System.out.println("Pedido enviado...");
-        System.out.println("Você comprou " + qtd + " computador(es).");
-    }
-}
